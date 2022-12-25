f4cc7278219d5b54dd3bb7697e284dec1a99ff0c
elenanij <elenanij@gmail.com>
2022-12-25 00:01:43 -0800

    Added imports to Common Methods since dependencies were merged

--- src/test/java/utils/CommonMethods.java
+++ src/test/java/utils/CommonMethods.java
@@ -1,5 +1,15 @@
 package utils;
 
+import io.cucumber.core.options.Constants;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 import java.util.concurrent.TimeUnit;
 
     public class CommonMethods {
