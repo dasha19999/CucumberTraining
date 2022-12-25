1ac99cb403df0823b46407b55495270abf4f08e6
elenanij <elenanij@gmail.com>
2022-12-24 23:34:32 -0800

    Built basic structure of Common Methods class

--- src/test/java/utils/CommonMethods.java
+++ src/test/java/utils/CommonMethods.java
@@ -1,6 +1,71 @@
 package utils;
 
-public class CommonMethods {
+import java.util.concurrent.TimeUnit;
 
-    //starting work on Common Methods
+    public class CommonMethods {
+
+        public static WebDriver driver;
+
+        public static void openBrowserAndLaunchApplication() {
+
+            ConfigReader.readProperties(Constants.CONFIGURATION_FILEPATH);
+
+            switch (ConfigReader.getPropertyValue("browser")) {
+                case "chrome" -> {
+                    WebDriverManager.chromedriver().setup();
+                    driver = new ChromeDriver();
+                }
+                case "firefox" -> {
+                    WebDriverManager.firefoxdriver().setup();
+                    driver = new FirefoxDriver();
+                }
+                default -> throw new RuntimeException("Invalid Browser name");
+            }
+
+            driver.manage().window().maximize();
+            driver.get(ConfigReader.getPropertyValue("url"));
+            driver.manage().timeouts().implicitlyWait(Constants.IMPLICIT_WAIT, TimeUnit.SECONDS);
+
+           //need to initialize Page Objects here when Page Initializer class is ready
+
+        }
+
+        public static void closeBrowser() {
+
+            driver.quit();
+        }
+
+        public static void sendText(WebElement element, String textToSend) {
+
+            element.clear();
+            element.sendKeys(textToSend);
+
+        }
+
+//to get webdriver wait
+
+        public static WebDriverWait createWait() {
+
+            WebDriverWait wait = new WebDriverWait(driver, Constants.EXPLICIT_WAIT);
+
+            return wait;
+        }
+
+        public static void waitToBeClickable(WebElement element){
+
+            createWait().until(ExpectedConditions.elementToBeClickable(element));
+        }
+
+        public static void click(WebElement element) {
+
+            waitToBeClickable(element);
+            element.click();
+        }
+
+        public static void selectDropDown(WebElement element, String text) {
+
+            Select sel = new Select(element);
+            sel.selectByVisibleText(text);
+        }
+
 }
