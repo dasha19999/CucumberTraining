2e5c1ff8a80739374111666365caca705dd0610c
elenanij <elenanij@gmail.com>
2022-12-25 16:02:04 -0800

    Deleted unnescesary import in Common method and added imports in Excel Reader

--- src/test/java/utils/CommonMethods.java
+++ src/test/java/utils/CommonMethods.java
@@ -1,6 +1,5 @@
 package utils;
 
-import io.cucumber.core.options.Constants;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
--- src/test/java/utils/ExcelReader.java
+++ src/test/java/utils/ExcelReader.java
@@ -1,5 +1,9 @@
 package utils;
 
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
