96d615aa833a09ca062bf5f7528cb9c824dfd43b
elenanij <elenanij@gmail.com>
2022-12-26 16:15:56 -0800

    Hooks succesfully added

---
+++ src/test/java/steps/Hooks.java
@@ -0,0 +1,19 @@
+package steps;
+
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
+import utils.CommonMethods;
+
+public class Hooks extends CommonMethods {
+
+    @Before
+    public void preCondition() {
+        openBrowserAndLaunchApplication();
+    }
+
+    @After
+    public void postCondition(Scenario scenario) {
+        closeBrowser();
+    }
+}
