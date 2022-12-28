1af5d024921bb1dd3ba1f92885f97722644106f3
elenanij <elenanij@gmail.com>
2022-12-28 15:54:44 -0800

    Added screensots to After Hook

--- src/test/java/steps/Hooks.java
+++ src/test/java/steps/Hooks.java
@@ -12,8 +12,21 @@ public class Hooks extends CommonMethods {
         openBrowserAndLaunchApplication();
     }
 
-  // @After
-  // public void postCondition(Scenario scenario) {
-  // closeBrowser();
-   // }
+    @After
+    public void postCondition(Scenario scenario) {
+
+        byte[] pic;
+        if (scenario.isFailed()) {
+
+            pic = takeScreenshot("failed/" + scenario.getName());
+        }
+        else {
+
+            pic = takeScreenshot("passed/" + scenario.getName());
+        }
+
+        scenario.attach(pic,"image/png", scenario.getName());
+        closeBrowser();
+    }
 }
+
